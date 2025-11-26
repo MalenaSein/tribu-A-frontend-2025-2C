1a6c6c1413c437792f91dcb03c6b6407994f6bb4
cl-fiuba <cleith@fi.uba.ar>
2025-11-26 15:34:07 -0300

    Agrega objetos necesarios para proveer el reporte de costos por proyecto.

--- src/APIClient.js
+++ src/APIClient.js
@@ -1,6 +1,8 @@
 const URL_BASE = "http://localhost:8080"
 const ENDPOINT_RECURSOS = "/resources"
+const ENDPOINT_PROYECTOS = "/projects"
 const ENDPOINT_TIME_ENTRIES_BY_RESOURCE = "/reports/resources"
+const ENDPOINT_REPORT_PROJECTS_RESOURCES_BY_YEAR = "/reports/projects"
 
 let APIClient = {
     getAllResources : async function () {
@@ -9,6 +11,14 @@ let APIClient = {
 
     getTimeEntriesByResource: async function(id) {
         return getData(URL_BASE + ENDPOINT_TIME_ENTRIES_BY_RESOURCE + "/" +id)
+    },
+
+    getAllProjects: async function() {
+        return getData(URL_BASE + ENDPOINT_PROYECTOS )
+    },
+
+    getResourcesByProjectAndYear: async function(projectId, year) {
+        return getData(URL_BASE + ENDPOINT_REPORT_PROJECTS_RESOURCES_BY_YEAR + "/" + projectId + "/" + year)
     }
 }
 
